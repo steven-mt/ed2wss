a4119a68dce8b13dc80ef59be1f89b9778b76914
steven-mt <80749704+steven-mt@users.noreply.github.com>
2024-01-24 13:25:20 +0700

    Extract function from click handler

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,107 +1,111 @@
-const textArea = document.querySelector<HTMLTextAreaElement>("#input-area")
-const convertButton = document.getElementById("convert-button")
-
 let inputString: string
+
 const selectedSets = new Set<string>()
 
-if (textArea && convertButton) {
-	const handleClick = () => {
-		if (inputString !== textArea.value) selectedSets.clear()
+const setReleases = new Set<string>()
 
-		inputString = textArea.value
+const parseInput = () => {
+	const textArea = document.querySelector<HTMLTextAreaElement>("#input-area")
+	if (!textArea) return
 
-		const inputLines = inputString.split("\n")
+	if (inputString !== textArea.value) selectedSets.clear()
 
-		const outputNumbers: string[] = []
+	inputString = textArea.value
 
-		const setReleases = new Set<string>()
+	const inputLines = inputString.split("\n")
 
-		inputLines.forEach((line) => {
-			const found = line.match(/.+\/.+(-.+)+.*/)
+	const outputNumbers: string[] = []
 
-			// TODO: reverse card order(character+event separate from climax cards)
-			// TODO: option to remove "E" prefix
+	inputLines.forEach((line) => {
+		const found = line.match(/.+\/.+(-.+)+.*/)
 
-			if (found) {
-				const foundLine = found[0]
+		// TODO: reverse card order(character+event separate from climax cards)
+		// TODO: option to remove "E" prefix
 
-				const foundLineSplit = foundLine.split("\t")
+		if (found) {
+			const foundLine = found[0]
 
-				const cardNumber = foundLineSplit[0]
-				if (!cardNumber) return
+			const foundLineSplit = foundLine.split("\t")
 
-				const amount = Number(foundLineSplit[1])
+			const cardNumber = foundLineSplit[0]
+			if (!cardNumber) return
 
-				if (isNaN(amount)) {
-					throw new Error(`No amount for ${cardNumber}`)
-				} else if (amount > 4)
-					throw new Error(`There are more than 4 cards of ${cardNumber}`)
+			const amount = Number(foundLineSplit[1])
 
-				const cardNumberSplit = cardNumber.split("-")
-				const setRelease = cardNumberSplit[0]
-				if (!setRelease) return
-				let individualNumber = cardNumberSplit[1]
-				if (!individualNumber) return
+			if (isNaN(amount)) {
+				throw new Error(`No amount for ${cardNumber}`)
+			} else if (amount > 4)
+				throw new Error(`There are more than 4 cards of ${cardNumber}`)
 
-				setReleases.add(setRelease)
+			const cardNumberSplit = cardNumber.split("-")
+			const setRelease = cardNumberSplit[0]
+			if (!setRelease) return
+			let individualNumber = cardNumberSplit[1]
+			if (!individualNumber) return
 
-				// Add "E" prefix or add number to set if the prefix already exists
-				if (individualNumber.startsWith("E")) {
-					selectedSets.add(setRelease)
-				} else if (selectedSets.has(setRelease)) {
-					if (individualNumber.startsWith("T")) {
-						individualNumber =
-							individualNumber[0] + "E" + individualNumber.slice(1)
-					} else {
-						individualNumber = "E" + individualNumber
-					}
-				}
+			setReleases.add(setRelease)
 
-				for (let index = 0; index < amount; index++) {
-					outputNumbers.push(`${setRelease}-${individualNumber}`)
+			// Add "E" prefix or add number to set if the prefix already exists
+			if (individualNumber.startsWith("E")) {
+				selectedSets.add(setRelease)
+			} else if (selectedSets.has(setRelease)) {
+				if (individualNumber.startsWith("T")) {
+					individualNumber =
+						individualNumber[0] + "E" + individualNumber.slice(1)
+				} else {
+					individualNumber = "E" + individualNumber
 				}
 			}
-		})
 
-		let outputString: string = ""
-		outputNumbers.forEach((outputNumber) => {
-			outputString = outputString.concat(outputNumber, "\n")
-		})
+			for (let index = 0; index < amount; index++) {
+				outputNumbers.push(`${setRelease}-${individualNumber}`)
+			}
+		}
+	})
 
-		const outputArea =
-			document.querySelector<HTMLTextAreaElement>("#output-area")
+	let outputString: string = ""
+	outputNumbers.forEach((outputNumber) => {
+		outputString = outputString.concat(outputNumber, "\n")
+	})
 
-		if (outputArea) outputArea.value = outputString
+	const outputArea = document.querySelector<HTMLTextAreaElement>("#output-area")
 
-		const container = document.querySelector("#checkboxContainer")
-		if (!container) return
-
-		container.innerHTML = ""
-
-		setReleases.forEach((setRelease, index) => {
-			const div = document.createElement("div")
-			div.style.display = "flex"
-			div.style.gap = "0.5rem"
-
-			const checkbox = document.createElement("input")
-			checkbox.type = "checkbox"
-			checkbox.id = `checkbox-${index}`
-			if (selectedSets.has(setRelease)) checkbox.checked = true
-			div.appendChild(checkbox)
-
-			const label = document.createElement("label")
-			label.htmlFor = `checkbox-${index}`
-			label.innerHTML = setRelease
-			div.appendChild(label)
-
-			container.appendChild(div)
-
-			checkbox.addEventListener("change", () => {
-				if (checkbox.checked) selectedSets.add(label.innerHTML)
-				else selectedSets.delete(label.innerHTML)
-			})
-		})
-	}
-
-	convertButton.addEventListener("click", () => handleClick())
+	if (outputArea) outputArea.value = outputString
 }
+
+const handleClick = () => {
+	parseInput()
+
+	const container = document.querySelector("#checkboxContainer")
+	if (!container) return
+
+	container.textContent = ""
+
+	setReleases.forEach((setRelease, index) => {
+		const div = document.createElement("div")
+		div.style.display = "flex"
+		div.style.gap = "0.5rem"
+
+		const checkbox = document.createElement("input")
+		checkbox.type = "checkbox"
+		checkbox.id = `checkbox-${index}`
+		if (selectedSets.has(setRelease)) checkbox.checked = true
+		div.appendChild(checkbox)
+
+		const label = document.createElement("label")
+		label.htmlFor = `checkbox-${index}`
+		label.innerHTML = setRelease
+		div.appendChild(label)
+
+		container.appendChild(div)
+
+		checkbox.addEventListener("change", () => {
+			if (checkbox.checked) selectedSets.add(label.innerHTML)
+			else selectedSets.delete(label.innerHTML)
+		})
+	})
+}
+
+const convertButton = document.getElementById("convert-button")
+
+if (convertButton) convertButton.addEventListener("click", () => handleClick())
